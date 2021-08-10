ce11f3d09dfd0b638fb3957a24a5f4e1589fc26e
azizuddinrahil <azizuddin.rahil123@gmail.com>
2021-08-10 08:56:21 +0530

    Added logo, title and desktop plans toggle on hover now

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ jQuery(function($) {
 	});
 
 	// Pricing Box Click Event
-	$('.pricing .box-main').click(function() {
+	$('.pricing .box-main').hover(function() {
 		$('.pricing .box-main').removeClass('active');
 		$('.pricing .box-second').removeClass('active');
 		$(this).addClass('active');
